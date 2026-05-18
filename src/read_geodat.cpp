b695919719b2a2551b25495aeb5b2aadced82a61
Hans Bihs <hans.bihs@gmail.com>
2026-05-18 13:04:35 -0500

    Update read_geodat.cpp

--- src/read_geodat.cpp
+++ src/read_geodat.cpp
@@ -176,8 +176,15 @@ void lexer::read_geodat()
         }
         
 
+         double Gx,Gy;
         for(n=0;n<G10;++n)
-        XYin(G10_x[n],G10_y[n]);
+        {
+        Gx = Xin(G10_x[n],G10_y[n]);
+        Gy = Yin(G10_x[n],G10_y[n]);
+        
+        G10_x[n] = Gx;
+        G10_y[n] = Gy;
+        }
 
         
     
